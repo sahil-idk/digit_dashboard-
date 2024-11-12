9bdf06b4ffb3922f6a20f74ce9c2db5eb0f2884d
sahil-idk <sahil.sahil@research.iiit.ac.in>
2024-11-12 13:02:19 +0530

    style fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ['@radix-ui/react-select', '@radix-ui/react-tabs', 'recharts'],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  experimental: {
+    optimizeCss: true, // Enable CSS optimization
+    optimizePackageImports: ['@radix-ui/react-select', '@radix-ui/react-tabs']
+  }
 };
 
 export default nextConfig;
